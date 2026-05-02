e34a5c35fdc68d7fb232f7c46824300656511382
Idiot <m-13175444@moe-dl.edu.my>
2026-05-02 17:29:44 +0800

    Update include/consts.h

--- include/consts.h
+++ include/consts.h
@@ -5,4 +5,23 @@
 #define MAX_ROWS 25
 #define VRAM_ADDRESS 0xB8000
 
+enum such_terminal_color {
+	BLACK = 0x00,
+    BLUE = 0x01,
+    GREEN = 0x02,
+    CYAN = 0x03,
+    RED = 0x04,
+    MAGENTA = 0x05,
+    BROWN = 0x06,
+    LIGHT_GREY = 0x07,
+    DARK_GREY = 0x08,
+    LIGHT_BLUE = 0x09,
+    LIGHT_GREEN = 0x0A,
+    LIGHT_CYAN = 0x0B,
+    LIGHT_RED = 0x0C,
+    LIGHT_MAGENTA = 0x0D,
+    LIGHT_BROWN = 0x0E,
+    WHITE = 0x0F
+}
+
 #endif
