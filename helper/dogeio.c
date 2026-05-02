415712cefb3eba4386360c369a8216ca975fab29
Idiot <m-13175444@moe-dl.edu.my>
2026-05-02 18:58:51 +0800

    Update helper/dogeio.c, though incomplete.

---
+++ helper/dogeio.c
@@ -0,0 +1,55 @@
+// include files
+#include <stddef.h>
+#include <stdint.h>
+#include <consts.h>
+
+// global variables
+int cursor_x = 0;
+int cursor_y = 0;
+uint8_t such_color = 0x0E;
+volatile char* video_buffer = (volatile char*) VRAM_ADRESS;
+
+// .basic functions that are needed for other useful functions.
+// set color of text
+void dogeio_setcolor(uint8_t color) {
+	such_color = color;
+}
+
+// to scroll down
+void dogeio_scroll() {
+   for (size_t y = 0; y < MAX_COLS - 1; y++) {
+   	for (size_t x = 0; x < MAX_ROWS; x++) {
+      	video_buffer[y * MAX_ROWS + x] = video_buffer[(y + 1) * VGA_ROWS + x];
+      }
+   }
+   
+   uint16_t blank = (uint16_t) 0x07 << 8 | (uint16_t) ' ';
+   for (size_t x = 0; x < MAX_ROWS; x++) {
+   	video_buffer[(MAX_COLS - 1) * MAX_ROWS + x] = blank;
+   }
+}
+
+// put a char in the screen
+void dogeio_putchar(char input, int x, int y) {
+	const int index = y * MAX_COLS + x;
+   video_buffer[index] = LIGHT_BROWN;
+}
+
+void dogeio_print(char* string) {
+	for (int i = 0; string[i] != '\0'; i++) {
+		if (string[i] == '\0') {
+         cursor_x = 0;
+         cursor_y++;
+      } else if () {}
+   }
+   
+      
+   
+
+void dogeio_clearscreen() {
+	for (int y = 0; x < MAX_COLS; y++) {
+    	for (int x = 0, y < MAX_ROWS, x++) {
+			putchar(' ', x, y);
+      }
+   }
+}
