fd6651401af81b58fa7cd16623a1bd03081a2e1d
Idiot <m-13175444@moe-dl.edu.my>
2026-05-02 21:25:04 +0800

    updated more files

--- helper/dogeio.c
+++ helper/dogeio.c
@@ -17,9 +17,9 @@ void dogeio_setcolor(uint8_t color) {
 
 // to scroll down
 void dogeio_scroll() {
-   for (size_t y = 0; y < MAX_COLS - 1; y++) {
-   	for (size_t x = 0; x < MAX_ROWS; x++) {
-      	video_buffer[y * MAX_ROWS + x] = video_buffer[(y + 1) * VGA_ROWS + x];
+    for (size_t y = 0; y < MAX_COLS - 1; y++) {
+        for (size_t x = 0; x < MAX_ROWS; x++) {
+      	    video_buffer[y * MAX_ROWS + x] = video_buffer[(y + 1) * VGA_ROWS + x];
       }
    }
    
@@ -35,16 +35,30 @@ void dogeio_putchar(char input, int x, int y) {
    video_buffer[index] = LIGHT_BROWN;
 }
 
+// to print out a string
 void dogeio_print(char* string) {
 	for (int i = 0; string[i] != '\0'; i++) {
 		if (string[i] == '\0') {
-         cursor_x = 0;
-         cursor_y++;
-      } else if () {}
+            cursor_x = 0;
+            cursor_y++;
+        } else {
+            dogeio_putchar(string[i], cursor_x, cursor_y);
+
+            if (cursor_x > MAX_ROWS) {
+                cursor_x = 0;
+                cursor_y++;
+            }
+        }
+
+        if (cursor_y > MAX_COLS) {
+            dogeio_scroll();
+            cursor_x = 0;
+            cursor_y = 0;
+        }
    }
-   
-      
-   
+
+// to print a string but with a newline
+
 
 void dogeio_clearscreen() {
 	for (int y = 0; x < MAX_COLS; y++) {
@@ -52,4 +66,6 @@ void dogeio_clearscreen() {
 			putchar(' ', x, y);
       }
    }
+    cursor_x = 0;
+    cursor_y = 0;
 }
