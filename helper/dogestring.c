9993263a59f12b371242aa81d366e36bb179367a
Idiot <m-13175444@moe-dl.edu.my>
2026-05-02 21:45:51 +0800

    udpated more files

--- helper/dogestring.c
+++ helper/dogestring.c
@@ -1,2 +1,24 @@
 // include files
-#include 
+#include <stddef.h>
+#include <stdint.h>
+#include <dogebool.h>
+
+// strlen
+void string_strlen(char* string) {
+
+}
+
+// strcmp
+int string_strcmp(char* string1, char* string2) {
+
+}
+
+// strncmp
+int string_strncmp(char* string1, char* string2) {
+    
+}
+
+// startswith
+bool string_startswith(char* string1, char* string2) {
+
+}
