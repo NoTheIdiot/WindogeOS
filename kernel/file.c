801a455f969031cfbff12fc4770476098d80cca0
NoTheIdiot <m-13175444@moe-dl.edu.my>
2026-05-27 21:52:14 +0800

    rewrite file functions

--- kernel/file.c
+++ kernel/file.c
@@ -1,10 +1,9 @@
-// include files
+// please fucking help me
 #include <stdint.h>
 #include <stddef.h>
 #include "string.h"
 #include "dogeio.h"
 
-// trying to make a file system
 typedef struct {
     char name[32];
     char file_extension[4];
@@ -22,7 +21,7 @@ vfs_file readme = {
         "This file system cannot be saved.",
         "This file system is now finally able to be writen!",
         "Enjoy your stay!",
-        NULL
+        ""
     }
 };
 
@@ -31,21 +30,17 @@ vfs_file dogescript_example = {
     .file_extension = "dsc",
     .content = {
         "PRINT HELLO WORLD",
-        NULL
+        ""
     }
 };
 
-// array of all files in the virtual file system
-vfs_file file_system[16] {NULL};
+vfs_file* file_system[16] = {NULL};
 
-// initialize file system
 void file_init_filesystem() {
-    file_system[0] = &readme,
-    file_system[1] = &dogescript_example,
-    NULL
+    file_system[0] = &readme;
+    file_system[1] = &dogescript_example;
 }
 
-// find a file by it's name
 vfs_file* file_find_by_name(const char* filename) {
     for (int i = 0; i < 16; i++) {
         if (file_system[i] == NULL) continue; 
@@ -57,13 +52,12 @@ vfs_file* file_find_by_name(const char* filename) {
     return NULL; 
 }
 
-// functions
 void file_read_file(const char* file) {
-    vfs_file file_var = file_find_by_name(file);
+    vfs_file* file_var = file_find_by_name(file);
     if (file_var == NULL) {
         dogeio_println("File isn't found. Pls try using much [dir]?");
     } else {
-        for (int i = 0; file_var->content[i] != NULL; i++) {
+        for (int i = 0; i < 64 && file_var->content[i][0] != '\0'; i++) {
             dogeio_println(file_var->content[i]);
         }
     }
@@ -73,43 +67,68 @@ void file_list_files() {
     dogeio_print("ID");
     dogeio_println("    FILES");
     dogeio_println("-----------");
-    for (int i = 0; file_system[i]->name != NULL; i++) {
-        dogeio_print(i); // id of a file
-        dogeio_print("    ");
-        dogeio_println(file_system[i]->name);
+    for (int i = 0; i < 16; i++) {
+        if (file_system[i] != NULL) {
+            dogeio_print_number(i);
+            dogeio_print("    ");
+            dogeio_println(file_system[i]->name);
+        }
     }
 }
 
-void file_rename_file(char* filename, char* new_name) {
-    vfs_file file_var = file_find_by_name(filename);
-    if (file_var == NULL) { dogeio_println("File not found, not wow. Try using many [dir]?"); return; }
-    file_var -> name = new_name;
+void file_rename_file(const char* filename, const char* new_name) {
+    vfs_file* file_var = file_find_by_name(filename);
+    if (file_var == NULL) { 
+        dogeio_println("File not found, not wow. Try using many [dir]?"); 
+        return; 
+    }
+    string_strncpy(file_var->name, new_name, sizeof(file_var->name) - 1);
+    file_var->name[sizeof(file_var->name) - 1] = '\0';
 }
 
-void file_write_file(char* filename, char* string) {
-    vfs_file file_var = file_find_by_name(filename);
-    if (file_var == NULL) { dogeio_println("File not found, not wow. Try using many [dir]?"); return; }
-    file_var -> content[]
+void file_write_file(const char* filename, const char* string) {
+    vfs_file* file_var = file_find_by_name(filename);
+    if (file_var == NULL) { 
+        dogeio_println("File not found, not wow. Try using many [dir]?"); 
+        return; 
+    }
+    
+    int i = 0;
+    while (i < 64 && file_var->content[i][0] != '\0') {
+        i++;
+    }
+    
+    if (i < 64) {
+        string_strncpy(file_var->content[i], string, sizeof(file_var->content[i]) - 1);
+        file_var->content[i][sizeof(file_var->content[i]) - 1] = '\0';
+        
+        if (i + 1 < 64) {
+            file_var->content[i + 1][0] = '\0';
+        }
+    } else {
+        dogeio_println("File full, much error!");
+    }
 }
 
-void file_delete_line(char* file[], int line) {
-    if (file == NULL || file[0] == NULL) {
+void file_delete_line(vfs_file* file, int line) {
+    if (file == NULL) {
         dogeio_print("Error: Invalid file\n");
         return;
     }
 
     int num_lines = 0;
-    for (int i = 1; file[i] != NULL; i++) {
+    while (num_lines < 64 && file->content[num_lines][0] != '\0') {
         num_lines++;
     }
 
-    if (line == 0 || line > num_lines) {
+    if (line < 0 || line >= num_lines) {
         dogeio_print("Error: Invalid line number\n");
         return;
     }
 
-    for (int i = line; i < num_lines; i++) {
-        file[i] = file[i + 1];
+    for (int i = line; i < num_lines - 1; i++) {
+        string_strcpy(file->content[i], file->content[i + 1]);
     }
-    file[num_lines] = NULL;
+    
+    file->content[num_lines - 1][0] = '\0';
 }
