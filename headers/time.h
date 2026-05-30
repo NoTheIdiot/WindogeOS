48ce5b0683b8a1c7b59d78ab92f804597d16ade2
NoTheIdiot <m-13175444@moe-dl.edu.my>
2026-05-30 20:12:29 +0800

    add time waiting functions

--- headers/time.h
+++ headers/time.h
@@ -2,8 +2,10 @@
 #define TIME_H
 
 void time_read_rtc(int reg);
+void time_rtc_handler();
 void time_update_time();
 void time_show();
 char* time_get_raw();
+void time_wait_ms(unsigned long ms, unsigned long tick_ms);
 
 #endif
