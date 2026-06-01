81310d796eb50de0c9d082a8edabf96db46e912b
NoTheIdiot <m-13175444@moe-dl.edu.my>
2026-06-01 23:24:12 +0800

    try to make tools for windows, grub hates windows.

--- tools/link.bat
+++ tools/link.bat
@@ -1,5 +1,4 @@
-ld.lld -m elf_i386 -T linker/linker.ld -o windoge.bin \
-    boot.o \
-    kernel.o idt.o data.o info.o serial.o dogescript.o \
-    dogeshell.o dogeio.o ports.o string.o suchboot.o time.o \
-    vbe.o dogeio_graphics.o font.o file.o
+@echo off
+pushd %~dp0\..
+ld.lld -m elf_i386 -T linker\linker.ld -o windoge.bin boot.o kernel.o idt.o data.o info.o serial.o dogescript.o dogeshell.o dogeio.o ports.o string.o suchboot.o time.o vbe.o dogeio_graphics.o font.o file.o
+popd
