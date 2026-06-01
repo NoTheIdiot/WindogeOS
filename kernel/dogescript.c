4d6b4fb7a489fb0ef683e571245949ca0b35e7f0
NoTheIdiot <m-13175444@moe-dl.edu.my>
2026-06-01 17:51:18 +0800

    added the history command, and dogescript v1.1

--- kernel/dogescript.c
+++ kernel/dogescript.c
@@ -3,22 +3,35 @@
 #include "dogeio.h"
 #include "string.h"
 #include "file.h"
-
+#include "dogeshell.h"
 
 // use extern for now
-void doge_script(char* file[]) {
-    if (file == NULL || file[0] == NULL) {
-        dogeio_print("Error: Invalid file\n");
-        return;
-    }
+void doge_script(vfs_file* file) {
+    for (int i = 1; i < 64; i++) {
+        char* line = file->content[i];
 
-    for (int i = 1; file[i] != NULL; i++) {
-        char* line = file[i];
+		if (line[0] == '\0') {
+			break;
+		}
+        
         if (string_startswith(line, "PRINT ")) {
             char* message = line + 6;
             
             // f##k this no need for qoutes
             dogeio_println(message);
+            
+        } else if (string_startswith(line, "BARK ")) {
+        	char* message = line + 5;
+        	dogeio_println(message);
+        	
+        } else if (string_startswith(line, "SHELL ")) {
+        	char* shell_command = line + 6;
+        	dogeshell_execute(shell_command);
+        	
+        } else {
+        	dogeio_print(line);
+        	dogeio_println(" <~ [1] command not found, not wow.");
+        	dogeio_println("Execution has stopped.");
         }
     }
 }
