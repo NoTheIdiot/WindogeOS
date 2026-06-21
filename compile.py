53692a877d44be97b756982df3ee18655fbc236b
NoTheIdiot <gohjunhee82666@outlook.com>
2026-06-21 21:53:34 +0800

    proceed to fail making it cross accessable

--- compile.py
+++ compile.py
@@ -6,9 +6,9 @@ import platform
 system = platform.system()
 
 if system == "Windows":
-    linker_var = "clang --target=i386-pc-none-elf -ffreestanding -nostdlib -fuse-ld=lld -Wl, -T, linker/linker.ld"
+    linker_var = "ld.lld -m elf_i386 -T linker/linker.ld"
 else:
-    linker_var = "ld -m elf-i386 -T linker/linker.ld"
+    linker_var = "ld -m elf_i386 -T linker/linker.ld"
 
 def run_cmd(command):
     return subprocess.run(command, shell=True)
