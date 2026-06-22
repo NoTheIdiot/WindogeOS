b8274fc9c700fb3231eb67f455aa41107d67cdf6
Idiot <gohjunhee82666@outlook.com>
2026-06-22 10:23:04 +0800

    deleted one piece of unused garbage

--- tools/link.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-pushd %~dp0\..
-ld.lld -m elf_i386 -T linker\linker.ld -o windoge.bin boot.o kernel.o idt.o data.o info.o serial.o dogescript.o dogeshell.o dogeio.o ports.o string.o suchboot.o time.o vbe.o dogeio_graphics.o font.o file.o
-popd
