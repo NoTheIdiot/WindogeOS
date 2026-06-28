bb41b2502196e7bd74653454081cc95b37ad49a5
NoTheidiot <gohjunhee82666@outlook.com>
2026-06-28 14:07:00 +0800

    oh shit it''s time for shit

--- kernel/data.c
+++
@@ -1,7 +0,0 @@
-// include files 
-#include <stddef.h>
-#include <stdint.h>
-
-// variables
-char* such_windoge_version = "WindogeOS V0.0.2 Build 0003 7/6/2026";
-char* such_windoge_version_short = "V0.0.2 Build 0003";
--- kernel/filesystem/filewrite.c
+++
@@ -1,4 +0,0 @@
-// this file is going to be used to write files, obviously.
-// include files
-#include <stddef.h>
-#include <stdint.h>
