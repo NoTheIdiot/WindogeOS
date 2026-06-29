6f8de30e1f0271745c2352af38edaa70678af8ce
NoTheidiot <gohjunhee82666@outlook.com>
2026-06-29 16:10:41 +0800

    made string functions

---
+++ headers/string.h
@@ -0,0 +1,28 @@
+#ifndef STRING_H
+#define STRING_H
+
+#include <stdint.h>
+#include <stddef.h>
+
+#ifndef restrict
+#define restrict __restrict
+#endif
+
+// memory stuff
+void *memcpy(void *restrict dest, const void *restrict src, size_t n);
+void *memset(void *s, int c, size_t n);
+void *memmove(void *dest, const void *src, size_t n);
+int memcmp(const void *s1, const void *s2, size_t n);
+
+// regular string stuff
+int string_strcmp(const char *string1, const char *string2);
+size_t string_strlen(const char* string);
+void string_itoa(int n, char* string);
+int string_strncmp(const char *string1, const char *string2, size_t n);
+int string_startswith(const char *string, const char *prefix);
+int string_atoi(char* s);
+void string_strcpy(char *dest, const char *src);
+char* string_strncpy(char* dest, const char* src, size_t n);
+char* string_strcat(char* dest, const char* src);
+
+#endif
--- helper/string/memory.c
+++ helper/string/memory.c
@@ -1,6 +1,6 @@
 #include <stdint.h>
 #include <stddef.h>
-#include "string.h"
+#include <string.h>
 
 // GCC and Clang reserve the right to generate calls to the following
 // 4 functions even if they are not directly called.
