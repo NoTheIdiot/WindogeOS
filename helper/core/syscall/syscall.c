7c7706967c5e1329001375d3e16218028b50d6db
NoTheIdiot <gohjunhee82666@outlook.com>
2026-09-09 11:42:36 +0800

    again

--- helper/core/syscall/syscall.c
+++ helper/core/syscall/syscall.c
@@ -1,29 +1,11 @@
 #include <system.h>
 #include <dogeio.h>
 #include <string.h>
-
-#define IA32_EFER  0xC0000080
-#define IA32_STAR  0xC0000081
-#define IA32_LSTAR 0xC0000082
-#define IA32_FMASK 0xC0000084
-
-extern void syscall_entry();
+#include <stdint.h>
 
 struct cpu_regs {
     uint64_t rax, rbx, rcx, rdx, rsi, rdi, rbp, r8, r9, r10, r11, r12, r13, r14, r15, user_rsp;
 };
-
-static inline uint64_t read_msr(uint32_t msr) {
-    uint32_t low, high;
-    __asm__ volatile("rdmsr" : "=a" (low), "=d" (high) : "c" (msr));
-    return ((uint64_t)high << 32) | low;
-}
-
-static inline void write_msr(uint32_t msr, uint64_t value) {
-    uint32_t low = value & 0xFFFFFFFF;
-    uint32_t high = value >> 32;
-    __asm__ volatile("wrmsr" : : "c" (msr), "a" (low), "d" (high));
-}
 
 uint64_t syscall_handler(struct cpu_regs *regs) {
     uint64_t sc_num = regs->rax;
@@ -35,7 +17,7 @@ uint64_t syscall_handler(struct cpu_regs *regs) {
             char* buffer   = (char*)regs->rsi;
             uint32_t size  = (uint32_t)regs->rdx;
 
-            ret_val = fs_read(filepath, buffer, size);
+            ret_val = (uint64_t)(int64_t)fs_read(filepath, buffer, size);
             break;
         }
 
@@ -43,7 +25,7 @@ uint64_t syscall_handler(struct cpu_regs *regs) {
             char* filepath = (char*)regs->rdi;
             char* buffer   = (char*)regs->rsi;
 
-            ret_val = fs_write(filepath, buffer);
+            ret_val = (uint64_t)(int64_t)fs_write(filepath, buffer);
             break;
         }
         
@@ -53,17 +35,4 @@ uint64_t syscall_handler(struct cpu_regs *regs) {
     }
 
     return ret_val;
-}
-
-void syscall_init(void) {
-    uint64_t efer = read_msr(IA32_EFER);
-    write_msr(IA32_EFER, efer | 1);
-
-    uint64_t star = 0;
-    star |= ((uint64_t)0x08 << 32); 
-    star |= ((uint64_t)0x13 << 48); 
-    write_msr(IA32_STAR, star);
-
-    write_msr(IA32_LSTAR, (uint64_t)syscall_entry);
-    write_msr(IA32_FMASK, 0x200);
 }
